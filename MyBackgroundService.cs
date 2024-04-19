e4754695f08d4b64f42e378c352fc7fd458d3463
manideep-bestshop <nmanideep1015@gmail.com>
2024-04-19 17:19:08 +0530

    service added

---
+++ MyBackgroundService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceExample
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MyBackgroundService> _logger;
+
+        public MyBackgroundService(ILogger<MyBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Background service is running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+        }
+    }
+}
